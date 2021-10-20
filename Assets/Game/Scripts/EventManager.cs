6f12bd2568d37ca54fb7e1a572dd24f17470db3d
xtedx <xtedx@users.noreply.github.com>
2021-10-20 20:56:45 +1100

    timer works, using observer pattern and c# events.

---
+++ Assets/Game/Scripts/EventManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TeddyToolKit.Core;
+using UnityEngine;
+
+namespace Game.Scripts
+{
+    /// <summary>
+    /// Manages Events in the game to reduce dependency, using Observer design pattern
+    /// The interested classes will register/listen and do action themselves
+    /// </summary>
+    public class EventManager : MonoSingleton<EventManager>
+    {
+        #region events declaration
+
+        //the events
+        public event Action OnTimerDone;
+        public event Action<int> OnTimer1SecondTick;
+
+        //the corresponding methods to broadcast called when event happens  
+        public void TriggerTimerDone()
+        {
+            //? means only call Invoke if Ontimer is not null, like if (x==null) 
+            OnTimerDone?.Invoke();
+        }
+
+        public void TriggerTimer1SecondTick(int id)
+        {
+            //? means only call Invoke if Ontimer is not null, like if (x==null) 
+            OnTimer1SecondTick?.Invoke(id);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Game/Scripts/UiManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using TeddyToolKit.Core;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+
+namespace Game.Scripts
+{
+    public class UiManager : MonoSingleton<UiManager>
+    {
+        public Text txtTimer;
+        
+        private void OnEnable()
+        {
+            RegisterListeners();
+        }
+
+        private void OnDisable()
+        {
+            DeregisterListeners();
+        }
+
+        /// <summary>
+        /// listen to the event and call method when that happens
+        /// </summary>
+        private void RegisterListeners()
+        {
+            EventManager.Instance.OnTimer1SecondTick += UpdateTimerText;
+        }
+        
+        /// <summary>
+        /// make sure to un-listen, always as a pair with the onenable
+        /// </summary>
+        private void DeregisterListeners()
+        {
+            EventManager.Instance.OnTimer1SecondTick -= UpdateTimerText;
+        }
+
+        /// <summary>
+        /// updates the text on the ui element
+        /// </summary>
+        /// <param name="value"></param>
+        private void UpdateTimerText(int value)
+        {
+            if (txtTimer) txtTimer.text = value.ToString();
+        }
+    }
+}
