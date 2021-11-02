2e5f66f2ca48a009d644a9be60e21c9cdb31bb28
xtedx <xtedx@users.noreply.github.com>
2021-11-02 23:05:24 +1100

    player control movement and camera works

--- Assets/Game/Scripts/PlayerController.cs
+++ Assets/Game/Scripts/PlayerController.cs
@@ -1,16 +1,112 @@
+using Mirror;
 using UnityEngine;
 
 namespace Game.Scripts
 {
-	public class PlayerController : MonoBehaviour
+	public class PlayerController : NetworkBehaviour
 	{
-		[SerializeField] private float speed = 5f;
+		[Header("Movement Settings")]
+		public float moveSpeed = 8f;
+		public float turnSensitivity = 5f;
+		public float maxTurnSpeed = 150f;
+
+		[Header("Diagnostics")]
+		public float horizontal;
+		public float vertical;
+		public float turn;
+		public float jumpSpeed;
+		public bool isGrounded = true;
+		public bool isFalling;
+		public Vector3 velocity;
+		
+		public CharacterController characterController;
+
+		void Start()
+		{
+			//only enable control if is local player
+			characterController.enabled = isLocalPlayer;
+		}
+		
+		public override void OnStartLocalPlayer()
+		{
+			Camera.main.orthographic = false;
+			Camera.main.transform.SetParent(transform);
+			Camera.main.transform.localPosition = new Vector3(0f, 3f, -8f);
+			Camera.main.transform.localEulerAngles = new Vector3(10f, 0f, 0f);
+		}
+		
+		void OnValidate()
+		{
+			if (characterController == null)
+				characterController = GetComponent<CharacterController>();
+		}
+		
+		/// <summary>
+		/// resets the camera when player is disabled
+		/// </summary>
+		void OnDisable()
+		{
+			if (isLocalPlayer && Camera.main != null)
+			{
+				Camera.main.orthographic = true;
+				Camera.main.transform.SetParent(null);
+				Camera.main.transform.localPosition = new Vector3(0f, 70f, 0f);
+				Camera.main.transform.localEulerAngles = new Vector3(90f, 0f, 0f);
+			}
+		}
 
 		// Update is called once per frame
-		private void Update()
+		void Update()
 		{
-			transform.position += transform.right * Time.deltaTime * speed * Input.GetAxis("Horizontal");
-			transform.position += transform.forward * Time.deltaTime * speed * Input.GetAxis("Vertical");
+			if (!isLocalPlayer)
+				return;
+
+			horizontal = Input.GetAxis("Horizontal");
+			vertical = Input.GetAxis("Vertical");
+
+			// Q and E cancel each other out, reducing the turn to zero
+			if (Input.GetKey(KeyCode.Q))
+				turn = Mathf.MoveTowards(turn, -maxTurnSpeed, turnSensitivity);
+			if (Input.GetKey(KeyCode.E))
+				turn = Mathf.MoveTowards(turn, maxTurnSpeed, turnSensitivity);
+			if (Input.GetKey(KeyCode.Q) && Input.GetKey(KeyCode.E))
+				turn = Mathf.MoveTowards(turn, 0, turnSensitivity);
+			if (!Input.GetKey(KeyCode.Q) && !Input.GetKey(KeyCode.E))
+				turn = Mathf.MoveTowards(turn, 0, turnSensitivity);
+
+			if (isGrounded)
+				isFalling = false;
+
+			if ((isGrounded || !isFalling) && jumpSpeed < 1f && Input.GetKey(KeyCode.Space))
+			{
+				jumpSpeed = Mathf.Lerp(jumpSpeed, 1f, 0.5f);
+			}
+			else if (!isGrounded)
+			{
+				isFalling = true;
+				jumpSpeed = 0;
+			}
+		}
+		
+		void FixedUpdate()
+		{
+			if (!isLocalPlayer || characterController == null)
+				return;
+
+			transform.Rotate(0f, turn * Time.fixedDeltaTime, 0f);
+
+			Vector3 direction = new Vector3(horizontal, jumpSpeed, vertical);
+			direction = Vector3.ClampMagnitude(direction, 1f);
+			direction = transform.TransformDirection(direction);
+			direction *= moveSpeed;
+
+			if (jumpSpeed > 0)
+				characterController.Move(direction * Time.fixedDeltaTime);
+			else
+				characterController.SimpleMove(direction);
+
+			isGrounded = characterController.isGrounded;
+			velocity = characterController.velocity;
 		}
 	}
 }
